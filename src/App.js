65d9969ef41cccfe5dc5bcd00ff34329a70b19e1
HaochongPan <a13126347151@gmail.com>
2022-08-14 12:39:14 +0800

    initial navbar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages'
+import { useStateContext } from './contexts/ContextProvider'
 function App() {
-  const activeMenu = true
+  const { activeMenu } = useStateContext()
   return (
     <div>
       <BrowserRouter>
@@ -29,7 +30,9 @@ function App() {
               <Sidebar />
             </div>
           )}
-          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`}>
+          <div className={activeMenu
+                ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2'}>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
